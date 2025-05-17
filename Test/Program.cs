1f60169a78d34ae058737f48a30cfafd9818d226
Chesnokov <118552281+6PateL@users.noreply.github.com>
2025-05-17 15:24:56 +0300

    upd

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,9 +1,11 @@
 ﻿using System;
 using System.Text;
 using System.Threading.Tasks;
+using System.Xml;
 using CursorLibrary.Controllers;
 using CursorLibrary.Models;
 using CursorLibrary.SD;
+using CursorLibrary.Utilities;
 
 class Program
 {
@@ -11,95 +13,55 @@ class Program
     {
         Console.OutputEncoding = Encoding.UTF8;
 
+        AppDomain.CurrentDomain.ProcessExit += CurrentDomain_ProcessExit;
+
         var cursorApi = CursorApiController.Default;
+        var keyboardApi = KeyboardApiController.Default;
 
-        cursorApi.OnMouseMoved += CursorApi_OnMouseMoved;
-        cursorApi.OnLeftMouseClicked += CursorApi_OnLeftMouseClicked;
-        cursorApi.OnRightMouseClicked += CursorApi_OnRightMouseClicked;
-        cursorApi.OnMousePulledUp += CursorApi_OnMousePulledUp;
-        cursorApi.OnMousePulledDown += CursorApi_OnMousePulledDown;
-        cursorApi.OnKeyPressed += CursorApi_OnKeyPressed;
+        cursorApi.OnMouseMoved += (s, e) => Console.WriteLine($"Курсор переміщено: X={e.PositionX}, Y={e.PositionY}");
+        cursorApi.OnLeftMouseClicked += (s, e) => Console.WriteLine("Клік лівою кнопкою виконано!");
+        cursorApi.OnRightMouseClicked += (s, e) => Console.WriteLine("Клік правою кнопкою виконано!");
+        cursorApi.OnMousePulledUp += (s, e) => Console.WriteLine($"Кнопку миші відпущено: {e.MouseType}");
+        cursorApi.OnMousePulledDown += (s, e) => Console.WriteLine($"Кнопку миші натиснуто: {e.MouseType}");
+        keyboardApi.OnKeyPressed += (s, e) => Console.WriteLine("Клавішу натиснуто!");
 
-        Console.WriteLine("Натискання клавіші...");
-        await cursorApi.SimulateKeyPressing(KeyboardKeys.A);
+        Console.WriteLine("Симуляція натискання клавіші A...");
+        await keyboardApi.SimulateKeyPressing(KeyboardKeys.A);
 
         if (Console.ReadKey(true).Key == ConsoleKey.A)
-            Console.WriteLine("Клавішу натиснуто!");
+        {
+            Console.WriteLine("кнопку A натиснуто");
+        }
 
-        await Task.Delay(TimeSpan.FromSeconds(5));
+        await Task.Delay(TimeSpan.FromSeconds(2));
 
-        Console.WriteLine("Переміщення миші...");
+        Console.WriteLine("Переміщення курсора до позиції (0, 0)...");
         await cursorApi.SetCursorPosition(0, 0);
 
-        await Task.Delay(TimeSpan.FromSeconds(5));
+        await Task.Delay(TimeSpan.FromSeconds(2));
 
-        Console.WriteLine("Клік лівою кнопкою миші...");
+        Console.WriteLine("Симуляція кліку лівою кнопкою...");
         await cursorApi.SimulateMouseClick(MouseType.LEFTMOUSE);
 
-        await Task.Delay(TimeSpan.FromSeconds(5));
+        await Task.Delay(TimeSpan.FromSeconds(2));
 
-        Console.WriteLine("Клік правою кнопкою миші...");
-        await cursorApi.SimulateMouseClick(MouseType.RIGHTMOUSE);
-
-        await Task.Delay(TimeSpan.FromSeconds(5));
-
-        Console.WriteLine("Натискання лівої кнопки миші (Pull Down)...");
-        await cursorApi.SimulateMousePullDown(MouseType.LEFTMOUSE);
-
-        await Task.Delay(TimeSpan.FromSeconds(5));
-
-        Console.WriteLine("Відпускання лівої кнопки миші (Pull Up)...");
-        await cursorApi.SimulateMousePullUp(MouseType.LEFTMOUSE);
-
-        await Task.Delay(TimeSpan.FromSeconds(5));
-
-        Console.WriteLine("Натискання правої кнопки миші (Pull Down)...");
+        Console.WriteLine("Симуляція натискання правої кнопки...");
         await cursorApi.SimulateMousePullDown(MouseType.RIGHTMOUSE);
 
-        await Task.Delay(TimeSpan.FromSeconds(5));
+        await Task.Delay(TimeSpan.FromSeconds(2));
 
-        Console.WriteLine("Відпускання правої кнопки миші (Pull Up)...");
+        Console.WriteLine("Симуляція відпускання правої кнопки...");
         await cursorApi.SimulateMousePullUp(MouseType.RIGHTMOUSE);
 
-        await Task.Delay(TimeSpan.FromSeconds(5));
+        await Task.Delay(TimeSpan.FromSeconds(2));
 
-        Console.WriteLine("Готово!");
+        Console.WriteLine("Демонстрація завершена! Натисніть будь-яку клавішу для виходу.");
         Console.ReadKey();
     }
 
-    private static void CursorApi_OnKeyPressed(object? sender, EventArgs e)
+    private static void CurrentDomain_ProcessExit(object? sender, EventArgs e)
     {
-        Console.WriteLine("Подію натискання клавіші викликано!");
-        Console.WriteLine();
-    }
-
-    private static void CursorApi_OnRightMouseClicked(object? sender, MouseInfoModel e)
-    {
-        Console.WriteLine("Подію кліку правою кнопкою миші викликано!");
-        Console.WriteLine();
-    }
-
-    private static void CursorApi_OnMouseMoved(object? sender, MouseInfoModel e)
-    {
-        Console.WriteLine("Подію руху миші викликано!");
-        Console.WriteLine();
-    }
-
-    private static void CursorApi_OnLeftMouseClicked(object? sender, MouseInfoModel e)
-    {
-        Console.WriteLine("Подію кліку лівою кнопкою миші викликано!");
-        Console.WriteLine();
-    }
-
-    private static void CursorApi_OnMousePulledUp(object? sender, MouseInfoModel e)
-    {
-        Console.WriteLine($"Подію відпускання кнопки миші викликано! Тип: {e.MouseType}");
-        Console.WriteLine();
-    }
-
-    private static void CursorApi_OnMousePulledDown(object? sender, MouseInfoModel e)
-    {
-        Console.WriteLine($"Подію натискання кнопки миші викликано! Тип: {e.MouseType}");
-        Console.WriteLine();
+        Console.WriteLine("Логи збережено");
+        Logger.ExportLogs("C:\\Users\\максим\\Desktop"); 
     }
 }
